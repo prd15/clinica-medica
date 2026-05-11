d60ac14885d4c6843166a2710d1b01c6610e80c0
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-11 10:22:00 -0300

    comenta findByAtivo no ConvenioRepository

--- commons/src/main/java/br/edu/imepac/commons/repositories/ConvenioRepository.java
+++ commons/src/main/java/br/edu/imepac/commons/repositories/ConvenioRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Repository
 public interface ConvenioRepository extends JpaRepository<ConvenioEntity, Long> {
 
+    // util pra filtrar so os ativos no dropdown do front, por exemplo
     List<ConvenioEntity> findByAtivo(Boolean ativo);
 }
 
