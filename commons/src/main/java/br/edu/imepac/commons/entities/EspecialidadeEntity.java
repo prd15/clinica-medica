3c10cd86ca76efdc600bea9e0e5ce127ca2ce5e8
Pedro Henrique Fernandes <pedrohenriquesilva.phf@gmail.com>
2026-05-11 15:24:55 -0300

    feat(especialidade): adiciona EspecialidadeEntity

---
+++ commons/src/main/java/br/edu/imepac/commons/entities/EspecialidadeEntity.java
@@ -0,0 +1,26 @@
+package br.edu.imepac.commons.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "especialidades")
+public class EspecialidadeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da especialidade é obrigatório")
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(length = 300)
+    private String descricao;
+}
---
+++ commons/src/main/java/br/edu/imepac/commons/repositories/EspecialidadeRepository.java
@@ -0,0 +1,4 @@
+package br.edu.imepac.commons.repositories;
+
+public class EspecialidadeRepository {
+}
