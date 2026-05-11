893c1988a7006ff248e1b74d7b787260c7db1148
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-11 08:58:00 -0300

    cria AlterarStatusRequest DTO

---
+++ administrativo/src/main/java/br/edu/imepac/administrativo/dtos/AlterarStatusRequest.java
@@ -0,0 +1,18 @@
+package br.edu.imepac.administrativo.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "Payload para alterar status do convênio")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlterarStatusRequest {
+
+    @Schema(description = "true para ativar, false para inativar", example = "false")
+    @NotNull(message = "Campo ativo é obrigatório")
+    private Boolean ativo;
+}
