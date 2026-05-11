3e68a202352c1bd39cd76f9c0f9cee54e2fa4616
Pedro Henrique Fernandes <pedrohenriquesilva.phf@gmail.com>
2026-05-11 16:23:08 -0300

    feat(especialidade): adiciona EspecialidadeResponse

---
+++ administrativo/src/main/java/br/edu/imepac/administrativo/dtos/EspecialidadeResponse.java
@@ -0,0 +1,15 @@
+package br.edu.imepac.administrativo.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EspecialidadeResponse {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+}
