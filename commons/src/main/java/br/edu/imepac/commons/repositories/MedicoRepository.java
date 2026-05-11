00bd473dcea474f591dcb467020df47f080622ad
Pedro Henrique Fernandes <pedrohenriquesilva.phf@gmail.com>
2026-05-11 16:25:09 -0300

    feat(medico): adiciona metodos findByCrm e findByAtivo no MedicoRepository

--- commons/src/main/java/br/edu/imepac/commons/repositories/MedicoRepository.java
+++ commons/src/main/java/br/edu/imepac/commons/repositories/MedicoRepository.java
@@ -4,6 +4,13 @@ import br.edu.imepac.commons.entities.MedicoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
+
+    Optional<MedicoEntity> findByCrm(String crm);
+
+    List<MedicoEntity> findByAtivo(Boolean ativo);
 }
