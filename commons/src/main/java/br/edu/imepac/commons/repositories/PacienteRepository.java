5849eb628ff2ef3e72d50dff47bbbf1fa6d747dc
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-11 09:35:00 -0300

    adiciona findByNome, findByCpf e findByConvenioId no PacienteRepository

--- commons/src/main/java/br/edu/imepac/commons/repositories/PacienteRepository.java
+++ commons/src/main/java/br/edu/imepac/commons/repositories/PacienteRepository.java
@@ -4,7 +4,15 @@ import br.edu.imepac.commons.entities.PacienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// por enquanto so usa os metodos do JpaRepository mesmo
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PacienteRepository extends JpaRepository<PacienteEntity, Long> {
+
+    List<PacienteEntity> findByNomeContainingIgnoreCase(String nome);
+
+    Optional<PacienteEntity> findByCpf(String cpf);
+
+    List<PacienteEntity> findByConvenioId(Long convenioId);
 }
