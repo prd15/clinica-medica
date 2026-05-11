bde30d5f8a82c8c85a7dc91002eb594fe73ecbec
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-11 08:47:00 -0300

    adiciona findByAtivo em ConvenioRepository

--- commons/src/main/java/br/edu/imepac/commons/repositories/ConvenioRepository.java
+++ commons/src/main/java/br/edu/imepac/commons/repositories/ConvenioRepository.java
@@ -4,7 +4,11 @@ import br.edu.imepac.commons.entities.ConvenioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConvenioRepository extends JpaRepository<ConvenioEntity, Long> {
+
+    List<ConvenioEntity> findByAtivo(Boolean ativo);
 }
 
