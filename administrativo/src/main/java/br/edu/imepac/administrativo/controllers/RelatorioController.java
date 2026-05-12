a4fc1c49a911cba1bfce08ee0042907bc75bb1d8
newtonfilho1310 <newtonfilho1310@gmail.com>
2026-05-12 00:27:54 -0300

    docs(swagger): document relatorios endpoints

--- administrativo/src/main/java/br/edu/imepac/administrativo/controllers/RelatorioController.java
+++ administrativo/src/main/java/br/edu/imepac/administrativo/controllers/RelatorioController.java
@@ -4,6 +4,10 @@ import br.edu.imepac.administrativo.dtos.ConsultaDiariaRelatorioResponse;
 import br.edu.imepac.administrativo.dtos.PacienteResponse;
 import br.edu.imepac.administrativo.dtos.PacientesPorConvenioRelatorioResponse;
 import br.edu.imepac.commons.services.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "Relatorios", description = "Relatorios gerenciais do administrativo")
 @RestController
 @RequestMapping("/v1/relatorios")
 public class RelatorioController {
@@ -27,16 +32,22 @@ public class RelatorioController {
         this.modelMapper = modelMapper;
     }
 
+    @Operation(summary = "Relatorio de consultas diarias")
+    @ApiResponse(responseCode = "200", description = "Relatorio retornado com sucesso")
     @GetMapping("/consultas-diarias")
     public ResponseEntity<ConsultaDiariaRelatorioResponse> consultasDiarias(
             @RequestParam
+            @Parameter(description = "Data do relatorio no formato yyyy-MM-dd", example = "2026-05-12")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             LocalDate data) {
         return ResponseEntity.ok(new ConsultaDiariaRelatorioResponse(data, 0));
     }
 
+    @Operation(summary = "Relatorio de pacientes por convenio")
+    @ApiResponse(responseCode = "200", description = "Relatorio retornado com sucesso")
     @GetMapping("/pacientes-por-convenio")
     public ResponseEntity<PacientesPorConvenioRelatorioResponse> pacientesPorConvenio(
+            @Parameter(description = "ID do convenio", example = "1")
             @RequestParam Long convenioId) {
         List<PacienteResponse> pacientes = pacienteService.findByConvenioId(convenioId)
                 .stream()
--- administrativo/src/main/java/br/edu/imepac/administrativo/dtos/ConsultaDiariaRelatorioResponse.java
+++ administrativo/src/main/java/br/edu/imepac/administrativo/dtos/ConsultaDiariaRelatorioResponse.java
@@ -1,16 +1,21 @@
 package br.edu.imepac.administrativo.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Schema(description = "Relatorio de consultas agrupadas por data")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConsultaDiariaRelatorioResponse {
 
+    @Schema(description = "Data consultada", example = "2026-05-12")
     private LocalDate data;
+
+    @Schema(description = "Total de consultas na data informada", example = "8")
     private Integer totalConsultas;
 }
