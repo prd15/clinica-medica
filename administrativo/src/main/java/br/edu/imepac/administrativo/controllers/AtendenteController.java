e3ad8f89e4dab713c33374d133ac320de50ea5af
newtonfilho1310 <newtonfilho1310@gmail.com>
2026-05-12 00:26:46 -0300

    docs(swagger): document atendente endpoints

--- administrativo/src/main/java/br/edu/imepac/administrativo/controllers/AtendenteController.java
+++ administrativo/src/main/java/br/edu/imepac/administrativo/controllers/AtendenteController.java
@@ -4,6 +4,10 @@ import br.edu.imepac.administrativo.dtos.AtendenteRequest;
 import br.edu.imepac.administrativo.dtos.AtendenteResponse;
 import br.edu.imepac.commons.entities.AtendenteEntity;
 import br.edu.imepac.commons.services.AtendenteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Atendentes", description = "Gerenciamento de atendentes e recepcionistas")
 @RestController
 @RequestMapping("/v1/atendentes")
 public class AtendenteController {
@@ -24,6 +29,12 @@ public class AtendenteController {
         this.modelMapper = modelMapper;
     }
 
+    @Operation(summary = "Cria novo atendente")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Atendente criado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados invalidos"),
+        @ApiResponse(responseCode = "409", description = "Usuario ja cadastrado")
+    })
     @PostMapping
     public ResponseEntity<AtendenteResponse> create(@Valid @RequestBody AtendenteRequest request) {
         try {
@@ -36,6 +47,8 @@ public class AtendenteController {
         }
     }
 
+    @Operation(summary = "Lista todos os atendentes")
+    @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
     @GetMapping
     public ResponseEntity<List<AtendenteResponse>> findAll() {
         List<AtendenteResponse> response = atendenteService.findAll()
@@ -45,6 +58,13 @@ public class AtendenteController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Atualiza atendente existente")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Atendente atualizado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados invalidos"),
+        @ApiResponse(responseCode = "404", description = "Atendente nao encontrado"),
+        @ApiResponse(responseCode = "409", description = "Usuario ja cadastrado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<AtendenteResponse> update(@PathVariable("id") Long id,
                                                     @Valid @RequestBody AtendenteRequest request) {
@@ -58,6 +78,11 @@ public class AtendenteController {
         }
     }
 
+    @Operation(summary = "Remove atendente por ID")
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Atendente removido com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Atendente nao encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         if (atendenteService.deleteById(id)) {
