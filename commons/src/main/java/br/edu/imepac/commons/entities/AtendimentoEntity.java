accd348131ac80eaf7793c53217cbe20cd5fdd61
alcantara-th <alcantara.th98@gmail.com>
2026-05-14 23:37:25 -0300

    feat: cria AtendimentoEntity com consultaId, medicoId e status

---
+++ commons/src/main/java/br/edu/imepac/commons/entities/AtendimentoEntity.java
@@ -0,0 +1,34 @@
+package br.edu.imepac.commons.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "atendimentos")
+public class AtendimentoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long consultaId;
+
+    @Column(nullable = false)
+    private Long medicoId;
+
+    @Column(nullable = false)
+    private Long pacienteId;
+
+    private LocalDateTime dataHora;
+
+    // ex: "REALIZADO", "EM_ANDAMENTO"
+    private String status;
+}
