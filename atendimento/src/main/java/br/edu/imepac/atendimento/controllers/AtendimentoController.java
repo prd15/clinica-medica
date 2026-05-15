997530f02ead8775c396f15250dc2772c9017910
alcantara-th <alcantara.th98@gmail.com>
2026-05-14 23:46:28 -0300

    feat: adiciona rota POST realizar atendimento

--- atendimento/src/main/java/br/edu/imepac/atendimento/controllers/AtendimentoController.java
+++ atendimento/src/main/java/br/edu/imepac/atendimento/controllers/AtendimentoController.java
@@ -10,8 +10,11 @@ import br.edu.imepac.commons.entities.AtendimentoEntity;
 import br.edu.imepac.commons.entities.ProntuarioEntity;
 import br.edu.imepac.commons.entities.SolicitacaoExameEntity;
 import br.edu.imepac.commons.services.AtendimentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +32,32 @@ public class AtendimentoController {
                                  AgendamentoClient agendamentoClient) {
         this.atendimentoService = atendimentoService;
         this.agendamentoClient = agendamentoClient;
+    }
+
+    @PostMapping
+    @Operation(summary = "Realiza atendimento e registra prontuário")
+    @ApiResponse(responseCode = "201", description = "Atendimento registrado")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    public ResponseEntity<AtendimentoResponse> realizar(@RequestBody @Valid AtendimentoRequest request) {
+        AtendimentoEntity entidade = new AtendimentoEntity();
+        entidade.setConsultaId(request.getConsultaId());
+        entidade.setMedicoId(request.getMedicoId());
+        entidade.setPacienteId(request.getPacienteId());
+
+        AtendimentoEntity salvo = atendimentoService.registrar(
+                entidade,
+                request.getDescricao(),
+                request.getDiagnostico(),
+                request.getObservacoes()
+        );
+
+        try {
+            agendamentoClient.confirmarRealizacao(request.getConsultaId());
+        } catch (Exception e) {
+            System.err.println("Aviso: nao foi possivel notificar o agendamento. " + e.getMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(toResponse(salvo, null));
     }
 
     private AtendimentoResponse toResponse(AtendimentoEntity entidade, Long prontuarioId) {
