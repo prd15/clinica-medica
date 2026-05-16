6018c17ce5721ebdf88bb029ca95578097f1d7c7
Pedro Henrique Fernandes <pedrohenriquesilva.phf@gmail.com>
2026-05-15 21:12:37 -0300

    feat(http): configura RestTemplate com HttpComponentsClientHttpRequestFactory para suportar PATCH

--- atendimento/src/main/java/br/edu/imepac/atendimento/config/AppConfig.java
+++ atendimento/src/main/java/br/edu/imepac/atendimento/config/AppConfig.java
@@ -2,6 +2,7 @@ package br.edu.imepac.atendimento.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,7 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        return new RestTemplate(factory);
     }
 }
