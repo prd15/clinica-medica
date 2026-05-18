515279c880564a24c964b88621802d1e4b7608b0
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-18 20:11:06 -0300

    feat(agendamento): adiciona @Slf4j no GlobalExceptionHandler para log estruturado

--- agendamento/src/main/java/br/edu/imepac/agendamento/config/GlobalExceptionHandler.java
+++ agendamento/src/main/java/br/edu/imepac/agendamento/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.edu.imepac.agendamento.config;
 
 import br.edu.imepac.agendamento.dtos.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
