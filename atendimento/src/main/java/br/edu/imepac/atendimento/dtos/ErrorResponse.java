c872e40c8f45f90d47d1a3164c269335b08bc535
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-18 19:43:33 -0300

    feat(atendimento): cria DTO padronizado de erro

---
+++ atendimento/src/main/java/br/edu/imepac/atendimento/dtos/ErrorResponse.java
@@ -0,0 +1,16 @@
+package br.edu.imepac.atendimento.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
