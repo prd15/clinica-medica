c68e1209f83688af41512a8230aa34c3d4390c09
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-19 00:31:25 -0300

    feat(administrativo): adiciona @Slf4j no GlobalExceptionHandler para log estruturado

--- administrativo/src/main/java/br/edu/imepac/administrativo/controllers/GlobalExceptionHandler.java
+++ administrativo/src/main/java/br/edu/imepac/administrativo/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.edu.imepac.administrativo.controllers;
 
 import br.edu.imepac.administrativo.dtos.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
