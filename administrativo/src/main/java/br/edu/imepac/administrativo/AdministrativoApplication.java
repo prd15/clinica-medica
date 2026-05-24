c459c029c0ecedbae7b56ddfaecb90b557f9fe71
Pedro Henrique Fernandes <pedrohenriquesilva.phf@gmail.com>
2026-05-24 17:56:23 -0300

    refactor(auditoria): BaseEntity sem PrePersist manual + @EnableJpaAuditing explicito em cada Application

--- administrativo/src/main/java/br/edu/imepac/administrativo/AdministrativoApplication.java
+++ administrativo/src/main/java/br/edu/imepac/administrativo/AdministrativoApplication.java
@@ -3,6 +3,7 @@ package br.edu.imepac.administrativo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // escopo restrito ao dominio administrativo: evita carregar entidades/repos/services
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EntityScan(basePackages = "br.edu.imepac.commons.entities.administrativo")
 @EnableJpaRepositories(basePackages = "br.edu.imepac.commons.repositories.administrativo")
+@EnableJpaAuditing
 public class AdministrativoApplication {
 
     public static void main(String[] args) {
